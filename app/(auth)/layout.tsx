7655e4a8b8586b757de2052a5e6fc07315d81504
Aditya Singh <aditya.singh@techment.com>
2024-06-13 18:37:33 +0530

    active btn Implementation

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,35 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navLink = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot Password", href: "/forgot-password" },
+];
+
+export default function authLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const pathname = usePathname();
+  return (
+    <>
+      {navLink.map((link) => {
+
+        let isActive = pathname.startsWith(link.href)
+        return (
+          <Link
+            className= {`p-3 ${isActive? "fw-bold text-blue-400 underline":"text-black-400"}`}
+            href={link.href}
+            key={link.name}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+      {children}
+    </>
+  );
+}
