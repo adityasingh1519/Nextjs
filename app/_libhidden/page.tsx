02786d31f7c31b0e622eadce5f7a3822eabc4924
Aditya Singh <aditya.singh@techment.com>
2024-06-14 13:40:47 +0530

    update page.tsx, not-found.tsx, not-found.tsx and page.tsx

--- app/_libhidden/page.tsx
+++ app/_libhidden/page.tsx
@@ -1,3 +1,3 @@
 export default function Pvt() {
-    return <h1>pvt can't be acces thorugh routing</h1>
+    return <h1>pvt can not  be acces thorugh routing</h1>
 }
--- app/products/[productId]/reviews/[reviewId]/page.tsx
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,22 @@
 import { notFound } from "next/navigation";
 
 import {Metadata} from 'next'
+import { error } from "console";
+
+
+function getRandomInt(count : number) : number {
+  return Math.floor(Math.random() * count );
+}
 
 export const metadata : Metadata = {
     title: 'reviews'
 }
 export default function ReviewId({ params }: { params: { reviewId: string } }) {
+  const random = getRandomInt(3);
+
+  if(random === 1) {
+    throw new Error('Loaing Review error');
+  }
   if (parseInt(params.reviewId) > 1000) {
     notFound();
   }
